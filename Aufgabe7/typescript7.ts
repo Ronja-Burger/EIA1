0a644cf7e34cdcb13c4de2dad03c08c85d5d40fb
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2021-11-28 11:06:17 +0100

    Aufgabe 7 so gut wie's ging vervollständigt, Flowchart erstellt

--- Aufgabe7/typescript7.ts
+++ Aufgabe7/typescript7.ts
@@ -1,26 +1,21 @@
-var sound:HTMLAudioElement = new Audio('C.mp3');
-sound.play();
+var samp = [new Audio('Audio/C.mp3'), new Audio('Audio/F.mp3'), new Audio('Audio/G.mp3'),
+new Audio('Audio/Hi_Hat.mp3'), new Audio('Audio/Kick.mp3'), new Audio('Audio/Snare1.mp3'),
+new Audio('Audio/Snare2.mp3'), new Audio('Audio/Laugh1.mp3'), new Audio('Audio/Laugh2.mp3'),
+new Audio('Audio/.mp3')];
 
-var sound:HTMLAudioElement = new Audio('F.mp3');
-sound.play();
+document.querySelector(".b1").addEventListener("click", function () { playSample(samp[0], "", ""); });
+document.querySelector(".b2").addEventListener("click", function () { playSample(samp[1], "", ""); });
+document.querySelector(".b3").addEventListener("click", function () { playSample(samp[2], "", ""); });
+document.querySelector(".b4").addEventListener("click", function () { playSample(samp[3], "", ""); });
+document.querySelector(".b5").addEventListener("click", function () { playSample(samp[4], "", ""); });
+document.querySelector(".b6").addEventListener("click", function () { playSample(samp[5], "", ""); });
+document.querySelector(".b7").addEventListener("click", function () { playSample(samp[6], "", ""); });
+document.querySelector(".b8").addEventListener("click", function () { playSample(samp[7], "", ""); });
+document.querySelector(".b9").addEventListener("click", function () { playSample(samp[8], "", ""); });
+document.querySelector(".play").addEventListener("click", function () { playSample(samp[4], samp[5], samp[6]); });
 
-var sound:HTMLAudioElement = new Audio('G.mp3');
-sound.play();
-
-var sound:HTMLAudioElement = new Audio('Hi_Hat.mp3');
-sound.play();
-
-var sound:HTMLAudioElement = new Audio('Kick.mp3');
-sound.play();
-
-var sound:HTMLAudioElement = new Audio('Snare1.mp3');
-sound.play();
-
-var sound:HTMLAudioElement = new Audio('Snare2.mp3');
-sound.play();
-
-var sound:HTMLAudioElement = new Audio('Laugh1.mp3');
-sound.play();
-
-var sound:HTMLAudioElement = new Audio('Laugh2.mp3');
-sound.play();
+function playSample(ton, snare, hihat) {
+    setInterval(function () { ton.play(); }, 0);
+    setInterval(function () { snare.play(); }, 1000);
+    setInterval(function () { hihat.play(); }, 2000);
+}

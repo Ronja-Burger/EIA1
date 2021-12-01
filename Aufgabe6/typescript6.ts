8da7d3493662515ca40494f78febf745f83e69a7
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2021-12-01 15:43:35 +0100

    Fehler Aufgabe 6 und 7 behoben

--- Aufgabe6/typescript6.ts
+++ Aufgabe6/typescript6.ts
@@ -1,116 +1,108 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
-var africa : string = "Africa";
-var southamerica : string = "South America";
-var europe : string = "Europe";
-var northamerica : string = "North America";
-var asia : string = "Asia";
-var australia : string = "Australia";
+    var africa: string = "Africa";
+    var southamerica: string = "South America";
+    var europe: string = "Europe";
+    var northamerica: string = "North America";
+    var asia: string = "Asia";
+    var australia: string = "Australia";
 
-var africa_2008 : number = 1280;
-var southamerica_2008 : number = 1132.6;
-var europe_2008 : number = 4956.7;
-var northamerica_2008 : number = 6600.4;
-var asia_2008 : number = 12954.7;
-var australia_2008 : number = 1993;
+    var africa_2008: number = 1280;
+    var southamerica_2008: number = 1132.6;
+    var europe_2008: number = 4956.7;
+    var northamerica_2008: number = 6600.4;
+    var asia_2008: number = 12954.7;
+    var australia_2008: number = 1993;
 
-var africa_2018 : number = 1235.5;
-var southamerica_2018 : number = 1261.5;
-var europe_2018 : number = 4209.3;
-var northamerica_2018 : number = 6035.6;
-var asia_2018 : number = 16274.1;
-var australia_2018 : number = 2100.5;
-
-
-/* Welt Gesamt Rechnung */
-
-var welt18 : number = africa_2018+southamerica_2018+europe_2018+northamerica_2018+asia_2018+australia_2018;
+    var africa_2018: number = 1235.5;
+    var southamerica_2018: number = 1261.5;
+    var europe_2018: number = 4209.3;
+    var northamerica_2018: number = 6035.6;
+    var asia_2018: number = 16274.1;
+    var australia_2018: number = 2100.5;
 
 
-/* Afrika Rechnung */
+    /* Welt Gesamt Rechnung */
 
-var afzuwelt : number = africa_2018/welt18*100;
-var afzuaf : number = (africa_2018/africa_2008-1)*100;
-var afzuafco: number = africa_2018-africa_2008;
-
-/* Süd Amerika Rechnung */
-
-var sazuwelt : number = southamerica_2018/welt18*100;
-var sazusa : number = (southamerica_2018/southamerica_2008-1)*100;
-var sazusaco: number = southamerica_2018-southamerica_2008;
-
-/* Europa Rechnung */
-
-var euzuwelt : number = europe_2018/welt18*100;
-var euzueu : number = (europe_2018/europe_2008-1)*100;
-var euzueuco: number = europe_2018-europe_2008;
-
-/* Nord Amerika Rechnung */
-
-var nazuwelt : number = northamerica_2018/welt18*100;
-var nazuna : number = (northamerica_2018/northamerica_2008-1)*100;
-var nazunaco: number = northamerica_2018-northamerica_2008;
-
-/* Asien Rechnung */
-
-var aszuwelt : number = asia_2018/welt18*100;
-var aszuas : number = (asia_2018/asia_2008-1)*100;
-var aszuasco: number = asia_2018-asia_2008;
-
-/* Australien Rechnung */
-
-var auszuwelt : number = australia_2018/welt18*100;
-var auszuaus : number = (australia_2018/australia_2008-1)*100;
-var auszuausco: number = australia_2018-australia_2008;
+    var welt18: number = africa_2018 + southamerica_2018 + europe_2018 + northamerica_2018 + asia_2018 + australia_2018;
 
 
-/* DOM-Manipulation*/
+    /* Afrika Rechnung */
 
-document.querySelector(".africa").addEventListener("click", function () {
-    lander(africa, africa_2018, afzuwelt, afzuaf, afzuafco);
-    var Grafik:HTMLElement = document.querySelector(".chart")
-    Grafik.style.height = afzuwelt + "px";
-});
+    var afzuwelt: number = africa_2018 / welt18 * 100;
+    var afzuaf: number = (africa_2018 / africa_2008 - 1) * 100;
+    var afzuafco: number = africa_2018 - africa_2008;
 
-document.querySelector(".southamerica").addEventListener("click", function () {
-    lander(southamerica, southamerica_2018, sazuwelt, sazusa, sazusaco);
-    var Grafik:HTMLElement = document.querySelector(".chart")
-    Grafik.style.height = sazuwelt + "px";
-});
+    /* Süd Amerika Rechnung */
 
-document.querySelector(".europe").addEventListener("click", function () {
-    lander(europe, europe_2018, euzuwelt, euzueu, euzueuco);
-    var Grafik:HTMLElement = document.querySelector(".chart")
-    Grafik.style.height = euzuwelt + "px";    
-});
+    var sazuwelt: number = southamerica_2018 / welt18 * 100;
+    var sazusa: number = (southamerica_2018 / southamerica_2008 - 1) * 100;
+    var sazusaco: number = southamerica_2018 - southamerica_2008;
 
-document.querySelector(".northamerica").addEventListener("click", function () {
-    lander(northamerica, northamerica_2018, nazuwelt, nazuna, nazunaco);
-    var Grafik:HTMLElement = document.querySelector(".chart")
-    Grafik.style.height = nazuwelt + "px";
-});
+    /* Europa Rechnung */
 
-document.querySelector(".asia").addEventListener("click", function () {
-    lander(asia, asia_2018, aszuwelt, aszuas, aszuasco);
-    var Grafik:HTMLElement = document.querySelector(".chart")
-    Grafik.style.height = aszuwelt + "px";
-});
+    var euzuwelt: number = europe_2018 / welt18 * 100;
+    var euzueu: number = (europe_2018 / europe_2008 - 1) * 100;
+    var euzueuco: number = europe_2018 - europe_2008;
 
-document.querySelector(".australia").addEventListener("click", function () {
-    lander(australia, australia_2018, auszuwelt, auszuaus, auszuausco);
-    var Grafik:HTMLElement = document.querySelector(".chart")
-    Grafik.style.height = auszuwelt + "px";
-});
+    /* Nord Amerika Rechnung */
 
-/* Funktion Länder */
+    var nazuwelt: number = northamerica_2018 / welt18 * 100;
+    var nazuna: number = (northamerica_2018 / northamerica_2008 - 1) * 100;
+    var nazunaco: number = northamerica_2018 - northamerica_2008;
 
-function lander(land, land18, landzuwelt, landzuland, landzulandco) {
-document.querySelector(".emission").innerHTML = land18.toFixed(2);
-document.querySelector(".relative").innerHTML = landzuwelt.toFixed(2) + "%";
-document.querySelector(".growth").innerHTML = landzuland.toFixed(2) + "%";
-document.querySelector(".growthrate").innerHTML = landzulandco.toFixed(2);
-document.querySelector("#titleRegion").innerHTML = land;
-document.querySelector("#subtitleRegion").innerHTML = land;
-}
+    /* Asien Rechnung */
+
+    var aszuwelt: number = asia_2018 / welt18 * 100;
+    var aszuas: number = (asia_2018 / asia_2008 - 1) * 100;
+    var aszuasco: number = asia_2018 - asia_2008;
+
+    /* Australien Rechnung */
+
+    var auszuwelt: number = australia_2018 / welt18 * 100;
+    var auszuaus: number = (australia_2018 / australia_2008 - 1) * 100;
+    var auszuausco: number = australia_2018 - australia_2008;
+
+
+    /* DOM-Manipulation*/
+
+    document.querySelector(".africa").addEventListener("click", function () {
+        lander(africa, africa_2018, afzuwelt, afzuaf, afzuafco);
+    });
+
+    document.querySelector(".southamerica").addEventListener("click", function () {
+        lander(southamerica, southamerica_2018, sazuwelt, sazusa, sazusaco);
+    });
+
+    document.querySelector(".europe").addEventListener("click", function () {
+        lander(europe, europe_2018, euzuwelt, euzueu, euzueuco);
+    });
+
+    document.querySelector(".northamerica").addEventListener("click", function () {
+        lander(northamerica, northamerica_2018, nazuwelt, nazuna, nazunaco);
+    });
+
+    document.querySelector(".asia").addEventListener("click", function () {
+        lander(asia, asia_2018, aszuwelt, aszuas, aszuasco);
+    });
+
+    document.querySelector(".australia").addEventListener("click", function () {
+        lander(australia, australia_2018, auszuwelt, auszuaus, auszuausco);
+    });
+
+    /* Funktion Länder */
+
+    
+
+    function lander(land, land18, landzuwelt, landzuland, landzulandco) {
+        document.querySelector(".emission").innerHTML = land18.toFixed(2);
+        document.querySelector(".relative").innerHTML = landzuwelt.toFixed(2) + "%";
+        document.querySelector(".growth").innerHTML = landzuland.toFixed(2) + "%";
+        document.querySelector(".growthrate").innerHTML = landzulandco.toFixed(2);
+        document.querySelector("#titleRegion").innerHTML = land;
+        document.querySelector("#subtitleRegion").innerHTML = land;
+        var Grafik: HTMLElement = document.querySelector(".chart")
+        Grafik.style.height = landzuwelt + "px";
+    }
 
 });
--- Aufgabe7/typescript7.js
+++ Aufgabe7/typescript7.js
@@ -3,15 +3,15 @@ window.addEventListener('load', function () {
         new Audio('Audio/Hi_Hat.mp3'), new Audio('Audio/Kick.mp3'), new Audio('Audio/Snare1.mp3'),
         new Audio('Audio/Snare2.mp3'), new Audio('Audio/Laugh1.mp3'), new Audio('Audio/Laugh2.mp3'),
         new Audio('Audio/.mp3')];
-    document.querySelector(".b1").addEventListener("click", function () { playSample(samp[0], "", ""); });
-    document.querySelector(".b2").addEventListener("click", function () { playSample(samp[1], "", ""); });
-    document.querySelector(".b3").addEventListener("click", function () { playSample(samp[2], "", ""); });
-    document.querySelector(".b4").addEventListener("click", function () { playSample(samp[3], "", ""); });
-    document.querySelector(".b5").addEventListener("click", function () { playSample(samp[4], "", ""); });
-    document.querySelector(".b6").addEventListener("click", function () { playSample(samp[5], "", ""); });
-    document.querySelector(".b7").addEventListener("click", function () { playSample(samp[6], "", ""); });
-    document.querySelector(".b8").addEventListener("click", function () { playSample(samp[7], "", ""); });
-    document.querySelector(".b9").addEventListener("click", function () { playSample(samp[8], "", ""); });
+    document.querySelector(".b1").addEventListener("click", function () { samp[0].play(); });
+    document.querySelector(".b2").addEventListener("click", function () { samp[1].play(); });
+    document.querySelector(".b3").addEventListener("click", function () { samp[2].play(); });
+    document.querySelector(".b4").addEventListener("click", function () { samp[3].play(); });
+    document.querySelector(".b5").addEventListener("click", function () { samp[4].play(); });
+    document.querySelector(".b6").addEventListener("click", function () { samp[5].play(); });
+    document.querySelector(".b7").addEventListener("click", function () { samp[6].play(); });
+    document.querySelector(".b8").addEventListener("click", function () { samp[7].play(); });
+    document.querySelector(".b9").addEventListener("click", function () { samp[8].play(); });
     document.querySelector(".play").addEventListener("click", function () { playSample(samp[4], samp[5], samp[6]); });
     function playSample(ton, snare, hihat) {
         setInterval(function () { ton.play(); }, 0);

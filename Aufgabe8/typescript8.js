5a1db0a894c995bbb7ca795e20118acc76770845
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2021-12-07 11:24:13 +0100

    Versuch, Aufgabe 8 zu fixen

--- Aufgabe8/typescript8.js
+++ Aufgabe8/typescript8.js
@@ -1,9 +1,13 @@
-// ja, es funktioniert noch nicht alles, aber ich habe moch für Montag schon mit Christian verabredet
+// funktioniert noch immer nicht, aber mit Fragen für Mittwoch versehen
 window.addEventListener("load", function () {
     var samp = [new Audio("Audio/C.mp3"), new Audio("Audio/F.mp3"), new Audio("Audio/G.mp3"),
         new Audio("Audio/Hi_Hat.mp3"), new Audio("Audio/Kick.mp3"), new Audio("Audio/Snare1.mp3"),
         new Audio("Audio/Snare2.mp3"), new Audio("Audio/Laugh1.mp3"), new Audio("Audio/Laugh2.mp3")];
-    document.querySelector(".b1").addEventListener("click", function () { playSample(samp[0]); });
+    var beat = [new Audio("Audio/Hi_Hat.mp3"), new Audio("Audio/Kick.mp3"), new Audio("Audio/Snare1.mp3"),
+        new Audio("Audio/Snare2.mp3")];
+    var i = 0;
+    var buttons = document.querySelector(".buttons");
+    document.querySelector(".b1").addEventListener("click", function () { playSample(samp[0]); }); // warum streicht er mir hier das rot an?
     document.querySelector(".b2").addEventListener("click", function () { playSample(samp[1]); });
     document.querySelector(".b3").addEventListener("click", function () { playSample(samp[2]); });
     document.querySelector(".b4").addEventListener("click", function () { playSample(samp[3]); });
@@ -13,46 +17,45 @@ window.addEventListener("load", function () {
     document.querySelector(".b8").addEventListener("click", function () { playSample(samp[7]); });
     document.querySelector(".b9").addEventListener("click", function () { playSample(samp[8]); });
     // die eine zentrale Funtion, mit der die Samples abgespielt werden
-    function playSample() {
-        for (let index = 0; index <= 8; index++) {
-            samp[index].play();
+    samp.forEach(playSample); // ich verstehe die hierzu abgegebene Erklärung nicht
+    function playSample(samp) {
+        while (i < beat.length) {
+            samp[i].play();
+            i++;
         }
     }
     // Play-Button (Beat)
-    document.querySelector(".fas fa-play-circle").addEventListener("click", function () { playBeat(); });
-    function playBeat() {
-        for (let index2 = 3; index2 <= 6; index2++) {
-            samp[index2].play();
-        }
-    }
-    // Tausch Play- gegen Stop-Button - noch zu klären
-    document.querySelector(".buttons2").setAttribute = class {
-    } = "fas fa-stop-circle";
-    id = "stop" > /i>);;
-    // Remix
-    function playSample() {
-        for (let index = (Math.random() * 100); index <= 100; index++) {
-            samp[index].play();
-        }
-    }
-    // Delete-Funktion
-    function deleteBeat() {
-        document.querySelector(".fas fa-trash-alt").addEventListener("click", function () { playInterval(); });
-        var index = 3;
-        var Interval = setInterval(playInterval, 250);
-        function playInterval() {
-            if (!playBeat) {
-                clearInterval(Interval);
+    document.querySelector(".fas fa-play-circle").addEventListener("click", function () {
+        playBeat(beat, 0); // was soll denn dann hier in die Klanmnmer rein?
+        function playBeat(beat) {
+            while (beat[i] == 0) {
+                beat[i].play();
+                i++;
             }
-            else if (index >= 8) {
-                index = 0;
-                playBeat([samp[index]]);
+        }
+        // Tausch Play- gegen Stop-Button
+        function Buttontausch() {
+            if (buttons.getAttribute("class") == "fas fa-stop-circle") {
+                buttons.setAttribute("class", "fas fa-play-circle");
             }
             else {
-                playBeat([samp[index]]);
+                buttons.setAttribute("class", "fas fa-stop-circle");
+                if (buttons.getAttribute("class") == "fas fa-play-circle") {
+                    beat.pause();
+                } // warum funktioniert das dann bei meinem Kommilitonen?
             }
-            index++;
+            // Remix
+            function remix(beat) {
+                for (var i = 0; i <= beat.length - 1; i++) {
+                    beat[i] = playBeat[Math.floor(Math.random() * 5)];
+                }
+            }
+            // Delete-Funktion
+            function deleteBeat() {
+                beat.length = 0;
+                i = 0;
+            }
         }
-    }
-});
+    });
+}); // was stimmt hier nicht? ist das Ende vom window.addEventlistener
 //# sourceMappingURL=typescript8.js.map
--- Aufgabe8/typescript8.ts
+++ Aufgabe8/typescript8.ts
@@ -1,72 +1,75 @@
-// ja, es funktioniert noch nicht alles, aber ich habe moch für Montag schon mit Christian verabredet
-
+// funktioniert noch immer nicht, aber mit Fragen für Mittwoch versehen
 window.addEventListener ("load", function () {
 
 var samp = [new Audio("Audio/C.mp3"), new Audio("Audio/F.mp3"), new Audio("Audio/G.mp3"),
 new Audio("Audio/Hi_Hat.mp3"), new Audio("Audio/Kick.mp3"), new Audio("Audio/Snare1.mp3"),
 new Audio("Audio/Snare2.mp3"), new Audio("Audio/Laugh1.mp3"), new Audio("Audio/Laugh2.mp3")];
 
-document.querySelector(".b1").addEventListener("click", function () {playSample(samp[0]); });
-document.querySelector(".b2").addEventListener("click", function () {playSample(samp[1]); });
-document.querySelector(".b3").addEventListener("click", function () {playSample(samp[2]); });
-document.querySelector(".b4").addEventListener("click", function () {playSample(samp[3]); });
-document.querySelector(".b5").addEventListener("click", function () {playSample(samp[4]); });
-document.querySelector(".b6").addEventListener("click", function () {playSample(samp[5]); });
-document.querySelector(".b7").addEventListener("click", function () {playSample(samp[6]); });
-document.querySelector(".b8").addEventListener("click", function () {playSample(samp[7]); });
-document.querySelector(".b9").addEventListener("click", function () {playSample(samp[8]); });
+var beat = [new Audio("Audio/Hi_Hat.mp3"), new Audio("Audio/Kick.mp3"), new Audio("Audio/Snare1.mp3"),
+new Audio("Audio/Snare2.mp3")];
+
+var i: number = 0;
+var buttons: HTMLElement = document.querySelector(".buttons");
+
+document.querySelector(".b1").addEventListener("click", function (): void {playSample(samp[0]); }); // warum streicht er mir hier das rot an?
+document.querySelector(".b2").addEventListener("click", function (): void {playSample(samp[1]); });
+document.querySelector(".b3").addEventListener("click", function (): void {playSample(samp[2]); });
+document.querySelector(".b4").addEventListener("click", function (): void {playSample(samp[3]); });
+document.querySelector(".b5").addEventListener("click", function (): void {playSample(samp[4]); });
+document.querySelector(".b6").addEventListener("click", function (): void {playSample(samp[5]); });
+document.querySelector(".b7").addEventListener("click", function (): void {playSample(samp[6]); });
+document.querySelector(".b8").addEventListener("click", function (): void {playSample(samp[7]); });
+document.querySelector(".b9").addEventListener("click", function (): void {playSample(samp[8]); });
 
 
 
 
 // die eine zentrale Funtion, mit der die Samples abgespielt werden
-function playSample () {
-for (let index: number = 0; index <= 8; index++) {
-    samp[index].play();
+samp.forEach(playSample); // ich verstehe die hierzu abgegebene Erklärung nicht
+function playSample (samp: 0): void {
+    while (i < beat.length) {
+    samp[i].play();
+    i++;
 }}
 
 
 
 
 // Play-Button (Beat)
-document.querySelector(".fas fa-play-circle").addEventListener("click", function () { playBeat(); });
-function playBeat () {
-    for (let index2: number = 3; index2 <= 6; index2++) {
-        samp[index2].play();
+document.querySelector(".fas fa-play-circle").addEventListener("click", function () { playBeat(beat: 0); // was soll denn dann hier in die Klanmnmer rein?
+                                                                                      function playBeat (beat: 0): void { // ich verstehe die automatische Behebung nicht - ist doch genau das Gleiche?
+    while (beat[i] == 0) {
+        beat[i].play();
+        i++;
     }}
 
 
 
 
-// Tausch Play- gegen Stop-Button - noch zu klären
-document.querySelector(".buttons2").setAttribute = (<i class= "fas fa-stop-circle" id = "stop"></i>);
-
+// Tausch Play- gegen Stop-Button
+                                                                                      function Buttontausch(): void {
+    if (buttons.getAttribute("class") == "fas fa-stop-circle") {
+        buttons.setAttribute("class", "fas fa-play-circle");
+    }
+    else {
+        buttons.setAttribute("class", "fas fa-stop-circle");
+        if (buttons.getAttribute("class") == "fas fa-play-circle") { beat.pause(); } // warum funktioniert das dann bei meinem Kommilitonen?
+    }
 
 
 
 // Remix
-function playSample () {
-    for (let index: number = (Math.random() * 100); index <= 100; index++) {
-        samp[index].play();
-    }}
+    function remix(beat): void {
+    for (var i: number = 0; i <= beat.length - 1; i++) {
+        beat[i] = playBeat[Math.floor(Math.random() * 5)];
+    }
+}
 
 
 
 // Delete-Funktion
-function deleteBeat() {
-    document.querySelector(".fas fa-trash-alt").addEventListener("click", function () { playInterval(); });
-    var index: number = 3;
-    var Interval: number = setInterval(playInterval, 250);
-    function playInterval() { 
-    if (!playBeat) {
-        clearInterval(Interval);
-    }
-    else if (index >= 8) {
-        index = 0;
-        playBeat([samp[index]]);
-    }
-    else {
-        playBeat([samp[index]]);
-    }  index++; }
+    function deleteBeat(): void {
+    beat.length = 0;
+    i = 0;
 }
-});
+}); // was stimmt hier nicht? ist das Ende vom window.addEventlistener
